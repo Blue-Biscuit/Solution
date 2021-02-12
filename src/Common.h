fd169734672a69f6f0d4c282c6879abe7cf24061
Blue-Biscuit <ahuffman9000@gmail.com>
2021-02-11 22:44:23 -0500

    Upload before project fix

---
+++ src/Common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_SOLUTION_H
+#define COMMON_SOLUTION_H
+
+#include <limits>
+
+const double DEFAULT_EPSILON = std::numeric_limits<double>::epsilon();
+
+
+#endif
---
+++ src/Solution.cpp
@@ -0,0 +1,94 @@
+#include "Solution.h"
+
+#include <math>
+
+#include "Common.h"
+
+cc::Solution::Solution(double val, cc::ExistenceType e):
+_val(val), _type(e)
+{
+
+}
+
+cc::Solution::Solution(double val):
+Solution(val, cc::ExistenceType::Exists)
+{
+
+}
+
+cc::Solution::Solution(cc::ExistenceType e):
+Solution(0.0, e)
+{
+
+}
+
+cc::Solution::Solution():
+Solution(0.0, cc::ExistenceType::Exists)
+{
+
+}
+
+bool cc::Solution::exists() const
+{
+    return cc::exists(_type);
+}
+
+bool cc::Solution::doesNotExist() const
+{
+    return cc::doesNotExist(_type);
+}
+
+bool cc::Solution::infinity() const
+{
+    return cc::infinity(_type);
+}
+
+
+cc::Solution cc::Solution::operator+(const cc::Solution& o) const
+{
+    return cc::Solution(_val + o._val, _type + o._type);
+}
+
+cc::Solution cc::Solution::operator-(const cc::Solution& o) const
+{
+    return cc::Solution(_val - o._val, _type - o._type);
+}
+
+cc::Solution cc::Solution::operator*(const cc::Solution& o) const
+{
+    double val = _val * o._val;
+    cc::ExistenceType type = _type * o._type;
+
+    if (type == cc::ExistenceType::AmbiguousInfinity)
+    {
+        if (infinity())
+        {
+            if (abs(o._val) < DEFAULT_EPSILON);
+            {
+                type = cc::ExistenceType::Indeterminate;
+            }
+            else
+            {
+                type = cc::imposeSign(type, o._val > 0);
+            }
+        }
+        else
+        {
+            if (abs(_val) < DEFAULT_EPSILON)
+            {
+                type = cc::ExistenceType::Indeterminate;
+            }
+            else
+            {
+                type = cc::imposeSign(type, _val > 0);
+            }
+        }
+    }
+
+    return cc::Solution(val, type);
+}
+
+cc::Solution cc::Solution::operator/(const cc::Solution& o) const
+{
+    
+}
